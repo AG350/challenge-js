cafba5fb6ee6d2bd06c8c33da1dfed03745aaf31
Alejandro Galeano <ale.gb32@gmail.com>
2022-03-18 11:24:16 -0300

    Create Auction event created, updated npm packages, deleted hello test function

---
+++ src/handlers/createAuction.js
@@ -0,0 +1,21 @@
+import { v4 as uuid } from 'uuid';
+
+
+async function createAuction(event, context) {
+  const { title } = JSON.parse(event.body);
+  const now = new Date();
+  const auction = {
+    id: uuid(),
+    title,
+    status: 'OPEN',
+    createdAt: now.toISOString(),
+  };
+  return {
+    statusCode: 201,
+    body: JSON.stringify({ auction }),
+  };
+}
+
+export const handler = createAuction;
+
+
