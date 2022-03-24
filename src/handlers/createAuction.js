553871b1d1474f8fd6f63ff6a55bdc7d99526b74
Alejandro Galeano <ale.gb32@gmail.com>
2022-03-24 19:45:33 -0300

    Create Auction function updated, tables are dynamic named

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,4 +1,7 @@
 import { v4 as uuid } from 'uuid';
+import AWS from 'aws-sdk';
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 
 async function createAuction(event, context) {
@@ -10,6 +13,10 @@ async function createAuction(event, context) {
     status: 'OPEN',
     createdAt: now.toISOString(),
   };
+  await dynamodb.put({
+    TableName: process.env.AUCTIONS_TABLE_NAME,
+    Item: auction,
+  }).promise();
   return {
     statusCode: 201,
     body: JSON.stringify({ auction }),
@@ -17,5 +24,3 @@ async function createAuction(event, context) {
 }
 
 export const handler = createAuction;
-
-
