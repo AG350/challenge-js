ea03566affa60d81055b1e95542fa1d24994119a
Alejandro Galeano <ale.gb32@gmail.com>
2022-04-19 22:27:56 -0300

    For upload, no changes

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,9 +1,7 @@
 import { v4 as uuid } from 'uuid';
 import AWS from 'aws-sdk';
-import middy  from '@middy/core'
-import httpJsonBodyParser  from '@middy/http-event-normalizer'
-import httpEventNormalizer from '@middy/http-error-handler'
-import httpErrorHandler from '@middy/http-json-body-parser'
+import commonMiddleware from '../lib/commonMiddleware';
+import createError from 'http-errors'
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
@@ -32,7 +30,4 @@ async function createAuction(event, context) {
   };
 }
 
-export const handler = middy(createAuction)
-  .use(httpJsonBodyParser())
-  .use(httpEventNormalizer())
-  .use(httpErrorHandler());
+export const handler = commonMiddleware(createAuction);
